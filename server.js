19ba8b291fae30e4d487b39f7c5552c35c0924ac
ade <125457226+Ade06AA@users.noreply.github.com>
2024-05-12 23:46:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import process from 'process';
+import routes from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use('/', routes);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
