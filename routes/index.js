bcf5d2cef156230702d95dcd8bacc9562cbc2c05
ade <125457226+Ade06AA@users.noreply.github.com>
2024-05-13 00:49:09 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { GetStatus, GetStats } from '/controllers/AppController';
+import {
+  GetStatus, GetStats, NewUser
+} from '/controllers/AppController';
 
 const router = express.Router();
 
 router.get('/status', GetStatus);
 router.get('/stats', GetStats);
+router.post('/users', NewUser);
 
 module.exports = router;
