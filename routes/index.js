fcb77c54d9dab66cfa2cec27e4f78df18f394603
ade <125457226+Ade06AA@users.noreply.github.com>
2024-05-12 23:53:20 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,9 @@
+import express from 'express';
+import { GetStatus, GetStats } from '/controllers/AppController';
 
+const router = express.Router();
+
+router.get('/status', GetStatus);
+router.get('/stats', GetStats);
+
+module.exports = router;
