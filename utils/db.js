df93723ff78fd5aa374f5ee4f47d42f2d2a3f335
ade <125457226+Ade06AA@users.noreply.github.com>
2024-05-12 22:50:52 +0100

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -2,8 +2,8 @@ import { MongoClient } from 'mongodb';
 import process from 'process';
 
 class DBClient{
-  let connection = false;
-  constructor (){
+  let connected = false;
+  constructor(){
     const host = process.env.DB_HOST || 'localhost';
     const port = parseInt(process.env.DB_PORT || '27017');
     const database = process.env.DB_DATABASE || 'files_manager';
@@ -11,14 +11,20 @@ class DBClient{
     const client= await new MongoClient(url);
     try {
       await client.connect();
-      this.connection = true;
-    } catch {
-      
+      this.connected = true;
+    } catch(err){
+      this.connected = false;
+      console.log(err);
     }
-    this.connection = true;
+    this.connected = true;
     this.colection = client.db(database).collection('documents');
   }
   isAlive(){
+    return this.connedted
+  }
+  async nbUsers(){
+  }
+  async nbFiles(){
   }
 }
 
