4cad861aa4598f8d15af49ff1a1f6602fedfbd18
ade <125457226+Ade06AA@users.noreply.github.com>
2024-05-13 02:43:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import routes from './routes/index';
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json()); // allow access to the res body object
 app.use('/', routes);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
